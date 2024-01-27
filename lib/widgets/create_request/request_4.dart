cc5941c0237417632779edc568cdfe455f91961b
Van Tam Nguyen <56034301+nguyentam2492k@users.noreply.github.com>
2024-01-28 02:54:40 +0700

    tạo file cho màn Request4 (Xem lại bài thi)

---
+++ lib/widgets/create_request/request_4.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Request4 extends StatefulWidget {
+  const Request4({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return Request4State();
+  }
+}
+
+class Request4State extends State<Request4> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
