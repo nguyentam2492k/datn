c38cbafa81bda4ff47f09758753ce608459f371c
Nguyen Tam <tamvai41@gmail.com>
2024-01-30 15:36:36 +0700

    làm ui cho Request 9 (Câp lại thẻ sinh viên)

--- lib/screens/create_request_screen/create_request_screen.dart
+++ lib/screens/create_request_screen/create_request_screen.dart
@@ -6,6 +6,7 @@ import 'package:datn/widgets/create_request/request_5.dart';
 import 'package:datn/widgets/create_request/request_6.dart';
 import 'package:datn/widgets/create_request/request_7.dart';
 import 'package:datn/widgets/create_request/request_8.dart';
+import 'package:datn/widgets/create_request/request_9.dart';
 import 'package:datn/widgets/create_request/welcome.dart';
 import 'package:datn/widgets/custom_widgets/bottom_sheet_with_list.dart';
 import 'package:flutter/material.dart';
@@ -53,7 +54,7 @@ class CreateRequestScreenState extends State<CreateRequestScreen> {
     const Request6(),
     const Request7(),
     const Request8(),
-    const Request3(),
+    const Request9(),
     const Request1(),
     const Request2(),
     const Request3(),
