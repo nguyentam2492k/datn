49316368d8d28f9d219f76ac79cb3f22fb750502
Van Tam Nguyen <56034301+nguyentam2492k@users.noreply.github.com>
2024-02-28 18:18:55 +0700

    thêm firebase vào project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:datn/screens/log_in/log_in.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future <void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
